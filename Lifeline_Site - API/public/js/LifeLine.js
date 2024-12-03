dee14e6eb144222184645831090adb9c58e0dc05
Bruno Araujo <bruno.maia@sptech.school>
2024-12-02 23:22:35 -0300

    Correção de Bugs

--- Lifeline_Site - API/public/js/LifeLine.js
+++ Lifeline_Site - API/public/js/LifeLine.js
@@ -1,4 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
     const btnVoltar = document.querySelector('#voltar');
     const btnCadastrar = document.querySelector('#btn_cadastrar');
     const btnAvancar = document.querySelector('#avançar');
@@ -10,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     btnAvancar.addEventListener("click", avancarPagina);
     btnVoltar.addEventListener("click", voltarPagina);
-    btnCadastrar.addEventListener("click", voltarPagina);
+    // btnCadastrar.addEventListener("click", voltarPagina);
 
     let paginaAtual = 1;
     const numeroDePaginas = 3;
@@ -73,11 +72,73 @@ document.addEventListener("DOMContentLoaded", function() {
                 } else {
                     abrirLivro()
                     papel3.classList.remove("virada");
-                    papel3.style.zIndex = 1
+                    setTimeout(() => {
+                        papel3.style.zIndex = 1
+                      }, 1000);
                     break;
                 }
             }
             paginaAtual--
         }
     }
-});
+
+function validarInputs(email,senha) {
+
+    
+    if(!emailValido(email)){
+        return alert("Email inválido. Email deve conter '@' e '.' após o arroba")
+    }
+    if(!senhaValida(senha)){
+        return alert("Senha inválida. Senha deve conter pelo menos um caractere especial\numa letra maiuscula e um numero\nE ter pelo menos 8 digitos")
+    }
+    return true
+    }
+     
+function emailValido(email){
+     
+    var validar = (email != "" && email.indexOf("@") != -1)
+    
+        if (validar){
+            for (var posicao = email.indexOf("@"); posicao < email.length; posicao++){
+            if(email[posicao] == "."){
+                return true
+            }
+            }
+            return false
+        }
+        return false
+    }
+     
+function senhaValida(senha){
+     
+       var caracteresLista = ["!","@","#","$","%","&","*","(",")","?","<",">"]
+       var validar = (senha != "" && (senha.length >=8 && senha.length <=32))
+       var temcaractere = false
+       var temNumero = false
+       if(validar){
+         for(var posicao = 0;posicao<senha.length;posicao++){
+           if(senha.includes(caracteresLista[posicao])){
+               temcaractere = true
+               break
+           }
+         }
+         
+         for (var posicao = 0;posicao<senha.length;posicao++){
+           if(senha[posicao] != isNaN(senha[posicao])){
+             temNumero = true
+             break
+           }
+         }
+         for (var posicao = 0;posicao<senha.length;posicao++){
+           if(senha[posicao] == senha[posicao].toUpperCase()){
+             temMaiuscula = true
+             break
+           }
+         }
+         if (temNumero && temcaractere && temMaiuscula){
+           return true
+         }
+         return false
+       }return false
+         
+}
